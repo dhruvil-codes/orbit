5732af4f932f03799e6a8c48946980530de31102
Dhruvil Mistry <dhruvilmistry16@gmail.com>
2026-08-07 23:49:31 +0530

    feat(research): package init

---
+++ backend/app/research/__init__.py
@@ -0,0 +1,7 @@
+"""
+Research Domain Package Init
+"""
+from app.research.company_research import CompanyResearchEngine
+from app.research.contact_research import ContactResearchEngine
+
+__all__ = ["CompanyResearchEngine", "ContactResearchEngine"]
